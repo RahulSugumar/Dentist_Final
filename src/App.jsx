8e77ae053ef0e1bd359ab3b37081998fc0226307
Rahul Sugumar <rahulsugumar1977@gmail.com>
2026-01-21 15:54:46 +0530

    Added scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
@@ -12,18 +13,21 @@ import Signup from './pages/Signup';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="services" element={<Services />} />
-        <Route path="clinic" element={<Clinic />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="booking" element={<Booking />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="clinic" element={<Clinic />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="booking" element={<Booking />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
